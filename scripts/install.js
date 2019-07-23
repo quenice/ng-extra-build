d7b7a7ea9bd545a0e7e6468ec6f1af5e66fa5ccb
Quenice <qiubing.it@gmail.com>
2019-07-23 17:19:33 +0800

    1.0.7

---
+++ scripts/install.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+function generateBuildConfFile() {
+    const source = "./node_modules/ng-extra-build/template/build.conf.json";
+    const dest = "./build.conf.json";
+    fs.copyFile(path.resolve(source),
+        path.resolve(dest),
+        fs.constants.COPYFILE_EXCL,
+        (err) => {
+            if (err) {
+                console.log(`Not copy template file from [${source}] to [${dest}]. Maybe has exists [${dest}]`);
+                return;
+            }
+
+            console.log(`Copied template file from [${source}] to [${dest}]`);
+        })
+}
+
+
+generateBuildConfFile();
